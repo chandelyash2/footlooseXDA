9c0cd3014b15b6324fde2b32be1aace2d89aa5a2
Yash <yash.codedrill@gmail.com>
2022-01-24 18:44:10 +0530

    add

--- client/components/layout/Header.tsx
+++ client/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import styles from '../../styles/globals.module.css'
 import Logo from '../../public/footloose.png'
 import Image from 'next/image';
@@ -6,15 +6,26 @@ import { WindowScroll } from '../../context';
 const Header = () => {
   const windowScroll = useContext(WindowScroll)
   console.log(windowScroll)
-  return <div className={styles.headerWrapper}>
-    <div className={!windowScroll?styles.headerContainer:styles.headerContainer1}>
-    <div className={styles.headerLogo}>
-      <Image src={Logo} alt="Logo" width="120px" height="60px"/>
-    </div>
-    </div>
-    
+  return (
 
-  </div>;
+    <div className={!windowScroll ? styles.headerContainer : styles.headerContainer1}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headerLogo}>
+          <Image src={Logo} alt="Logo" width="120px" height="60px" />
+        </div>
+        <div className={styles.headerContent}>
+          <h4>Explore</h4>
+          <h4>Places</h4>
+          <h4>Blog</h4>
+
+        </div>
+      
+
+      </div>
+    </div>
+  )
+
+
 };
 
 export default Header;
